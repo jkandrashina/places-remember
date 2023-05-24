7ec1fe526ca99a3a0cacc743274dd3674e2dcb33
jkandrashina <curiouswebfox@gmail.com>
2023-05-24 23:18:10 +0500

    create PlaceRemember model and migration file

--- places_app/models.py
+++ places_app/models.py
@@ -1,3 +1,29 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class PlaceRemember(models.Model):
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        verbose_name='Автор',
+    )
+    place_name = models.CharField(
+        max_length=150,
+        verbose_name='Название'
+    )
+    comment = models.TextField(
+        verbose_name='Комментарий'
+    )
+    pub_date = models.DateField(
+        auto_now_add=True,
+        verbose_name='Дата'
+    )
+    
+
+    def __str__(self):
+        return self.place_name
+    
+    class Meta:
+        verbose_name = 'Воспоминание'
+        verbose_name_plural = 'Воспоминания'
