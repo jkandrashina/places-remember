e19adde5b5eaecf3e69b964a1ef42c1a7adfa532
jkandrashina <curiouswebfox@gmail.com>
2023-05-25 13:11:43 +0500

    update: add url for add-place page

--- places_app/urls.py
+++ places_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import HomeView
+from .views import HomeView, PlaceCreateView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
+    path('add-place', PlaceCreateView.as_view(), name='add_place')
 ]
