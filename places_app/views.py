b09ca1db764212154210938de91d225afcda880d
jkandrashina <curiouswebfox@gmail.com>
2023-05-27 12:53:14 +0500

    update: add PlacesListView, change PlaceCreateView
    1. Add view for "my-places" page.
    2. Сhange success_url: after submitting "add-place" form user is redirected to "my-places" page. Previously user was redirected to "home" page

--- places_app/views.py
+++ places_app/views.py
@@ -1,5 +1,6 @@
 #from django.shortcuts import render
 from django.views.generic.base import TemplateView
+from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView
 from django.urls import reverse_lazy
 
@@ -10,10 +11,19 @@ class HomeView(TemplateView):
     template_name = 'places_app/index.html'
 
 
+class PlacesListView(ListView):
+    model = PlaceRemember
+    template_name = 'places_app/my-places.html'
+    context_object_name = 'places'
+
+    def get_queryset(self):
+        return PlaceRemember.objects.filter(author=self.request.user)
+
+
 class PlaceCreateView(CreateView):
     model = PlaceRemember
     template_name = 'places_app/add-place.html'
-    success_url = reverse_lazy('home')
+    success_url = reverse_lazy('my_places')
     fields = ['place_name', 'comment']
     
     def form_valid(self, form):
