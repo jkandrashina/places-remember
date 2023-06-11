bbdf9c8290e0dd8ab583b9c81be5faf55cbf553d
jkandrashina <curiouswebfox@gmail.com>
2023-06-11 16:24:47 +0500

    add "map" widget to Django admin
    This widget displays a map in "location" field of "PlaceRemember" table in edit mode

--- places_app/admin.py
+++ places_app/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 from .models import PlaceRemember
+from treasuremap import widgets
+
 
 @admin.register(PlaceRemember)
 class PlaceAdmin(admin.ModelAdmin):
     list_display = ('place_name', 'author', 'pub_date')
+
+    def formfield_for_dbfield(self, db_field, **kwargs):
+        if db_field.name == 'location':
+            kwargs['widget'] = widgets.AdminMapWidget()
+        return super(PlaceAdmin, self).formfield_for_dbfield(db_field, **kwargs)
